5172dcff103e4a96ba2ad977e37f78c59d5a904e
Saif-eddine Houir <saifeddinehouir01@gmail.com>
2024-09-08 04:08:01 +0200

    Delete shiptracking directory

--- shiptracking/src/main/java/com/example/shiptracking/repository/VesselRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.shiptracking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.shiptracking.model.Vessel;
-
-public interface VesselRepository extends JpaRepository<Vessel, Long> {
-    Vessel findByMmsi(int mmsi);
-}
